33bb0801a46b0ee6d04a28a596f0a2ca2a7ff853
Afiz Savage <savageafiz9@gmail.com>
2024-09-03 23:24:20 +0000

    add root layout header and navigation

---
+++ components/Offices.tsx
@@ -0,0 +1,44 @@
+import clsx from 'clsx'
+
+function Office({
+    name,
+    children,
+    invert = false,
+}: {
+    name: string
+    children: React.ReactNode
+    invert?: boolean
+}) {
+    return (
+        <address
+            className={clsx(
+                'text-sm not-italic',
+                invert ? 'text-neutral-300' : 'text-neutral-600',
+            )}
+        >
+            <strong className={invert ? 'text-white' : 'text-green-900'}>
+                {name}
+            </strong>
+            <br />
+            {children}
+        </address>
+    )
+}
+
+export function Offices({
+    invert = false,
+    ...props
+}: React.ComponentPropsWithoutRef<'ul'> & { invert?: boolean }) {
+    return (
+        <ul role="list" {...props}>
+            <li>
+                <Office name="Freetown" invert={invert}>
+                    37 Fraser Street, Off Wilkinson Road,
+                    <br />
+                    Freetown, Sierra Leone
+                </Office>
+            </li>
+
+        </ul>
+    )
+}
